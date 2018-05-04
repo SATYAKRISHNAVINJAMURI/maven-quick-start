70b42d48db999ec8fdd7fa1f29b76160d59151c8
SATYAKRISHNAVINJAMURI <satyakrishnavinjamuri868@gmail.com>
2018-05-04 12:31:12 +0530

    Added Dependencies:

--- src/main/java/org/intern/training/Application.java
+++ src/main/java/org/intern/training/Application.java
@@ -1,7 +1,24 @@
 package org.intern.training;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
     
+    public int countWords(String words){
+    	String[] seperateWords = StringUtils.split(words,' ');
+    	return (seperateWords == null) ? 0 : seperateWords.length;
+    }
+
+    public void greet(){
+    	List<String> greetings = new ArrayList<>();
+    	greetings.add("Hello");
+
+    	for(String greeting : greetings){
+    		System.out.println("Greeting: "  + greeting);
+    	}
+    }
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -10,5 +27,8 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+    app.greet();
+    int count = app.countWords("I have Four Words");
+    System.out.println("Word Count: " + count);
     }
 }
